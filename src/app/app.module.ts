5f6855589dbefebcfd7cd7042386cc8a78bfd665
Luc Bertrand <luc.bertrand@canada.ca>
2017-03-03 14:38:36 -0500

    hard code query search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgGridModule } from "ag-grid-angular/main";
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainInterfaceComponent } from './main-interface/main-interface.component';
+import { QueryViewComponent } from './query-view/query-view.component';
+import { RestangularModule } from 'ng2-restangular';
+
+const appRoutes:Routes=[
+	{
+		path:'main',
+	   	component: MainInterfaceComponent,
+   		data: {breadcrumb: 'mybreadcrumb'}
+	},
+	{
+		path: 'query',
+	   	component: QueryViewComponent,
+		data: {title: 'My Query Title'}
+	},
+	{
+		path: '',
+		redirectTo: '/query',
+		pathMatch: 'full'
+	}
+];
+
+export function restangular(RestangularProvider){
+	RestangularProvider.setBaseUrl('http://localhost:8080/');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainInterfaceComponent,
+    QueryViewComponent
   ],
   imports: [
     BrowserModule,
+	AgGridModule.withComponents(
+		[MainInterfaceComponent]
+	),
     FormsModule,
-    HttpModule
+    HttpModule,
+	RouterModule.forRoot(appRoutes),
+	RestangularModule.forRoot(restangular),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -13,7 +13,7 @@ export class QueryService {
 	}
 
 	getGroups(){
-		let element = this.restangular.one("fm2/service/foods").get();
+		let element = this.restangular.one("fm/service/foods?data-source=both&food-recipe-name=&food-recipe-code=&commit-date-begin=&commit-date-end=&cnf-code=&subgroup-code=&cfg-tier=0&recipe=0&sodium=0&sugar=0&fat=0&transfat=0&caffeine=0&free-sugars=0&sugar-substitutes=0&reference-amount-missing=on&cfg-serving-missing=on&tier-4-serving-missing=on&energy-value-missing=on&cnf-code-missing=on&recipe-rolled-up-down-missing=on&sodium-value-missing=on&sugar-value-missing=on&fat-value-missing=on&transfat-value-missing=on&satfat-value-missing=on&select-all-missing=on&added-sodium-missing=on&added-sugar-missing=on&added-transfat-missing=on&added-caffeine-missing=on&added-free-sugars-missing=on&added-sugar-substitutes-missing=on").get();
 		return element;
 	}
 }
