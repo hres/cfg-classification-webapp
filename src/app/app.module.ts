ecd9750a68ce159e6363aca037b1331bca19320a
Luc Bertrand <luc.bertrand@canada.ca>
2017-05-10 09:22:37 -0400

    Refactor routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AgGridModule } from "ag-grid-angular/main";
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
 import { RestangularModule } from 'ng2-restangular';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { MainInterfaceComponent } from './main-interface/main-interface.component';
-import { QueryViewComponent } from './query-view/query-view.component';
+import { MainInterfaceComponent }	from './main-interface/main-interface.component';
+import { QueryViewComponent }		from './query-view/query-view.component';
 import { SaveViewComponent } from './save-view/save-view.component';
 import { DatasetsComponent } from './datasets/datasets.component';
 import { DatasetsActionComponent } from './datasets/datasets-action/datasets-action.component';
@@ -18,26 +17,7 @@ import { NumericEditorComponent } from './main-interface/numeric-editor/numeric-
 import { BooleanEditorComponent } from './main-interface/boolean-editor/boolean-editor.component';
 import { StringEditorComponent } from './main-interface/string-editor/string-editor.component';
 
-const appRoutes:Routes=[
-	{
-		path:'datasets',
-		component: DatasetsComponent
-	},
-	{
-		path:'main/:id',
-	   	component: MainInterfaceComponent,
-	},
-	{
-		path: 'query/:env',
-	   	component: QueryViewComponent,
-		data: {env: 'My Query Title'}
-	},
-	{
-		path: '',
-		redirectTo: '/datasets',
-		pathMatch: 'full'
-	}
-];
+import { AppRoutingModule }	from './app-routing.module';
 
 // Do your Restangular default settings here
 export function restangularConfigFactory(RestangularProvider){}
@@ -67,9 +47,9 @@ export function restangularConfigFactory(RestangularProvider){}
 			StringEditorComponent
 		]
 	),
+	AppRoutingModule,
     FormsModule,
     HttpModule,
-	RouterModule.forRoot(appRoutes),
 	RestangularModule.forRoot(restangularConfigFactory),
 	MaterialModule
   ],
