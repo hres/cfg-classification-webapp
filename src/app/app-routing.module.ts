4dad768b41d3760057a5532d03392a222035a2ca
Luc Bertrand <luc.bertrand@canada.ca>
2018-02-22 10:39:02 -0500

    remove routing debug statements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,7 +42,7 @@ const appRoutes:Routes =[
 
 @NgModule({
 	imports:[
-		RouterModule.forRoot(appRoutes, { enableTracing:true })
+		RouterModule.forRoot(appRoutes, { enableTracing:false })
 	],
 	exports:[
 		RouterModule
